7ac9cadb40097f1885f12c79999fba2ce2953574
slnt_opp <nik.ivanovskij@gmail.com>
2017-05-03 12:59:47 +0300

    IPProvider и AdminController docs

--- opennebula_connectivity/src/Controller/AdminController.php
+++ opennebula_connectivity/src/Controller/AdminController.php
@@ -3,7 +3,7 @@
 
 namespace OneCS\Controller;
 
-use TinyApp\Controller\AbstractController;
+use TinyApp\Controller\AbstractController; // vendor/iddqdby/tinyapp
 
 
 class AdminController extends AbstractController {
@@ -12,47 +12,47 @@ class AdminController extends AbstractController {
 
         $data = [];
 
-        $tags = trim( $tags );
-        $tags = empty( $tags )
-            ? []
-            : array_map( function ( $tag ) {
+        $tags = trim( $tags ); //Удаление из строки пробелов и др символов из начала и конца строки
+        $tags = empty( $tags ) //Проверка на пустую строку
+            ? [] //Задается массив
+            : array_map( function ( $tag ) { //array_map возвращает массив(второй аргумент) обработанный функцией tag
                 return strtolower( trim( $tag ) );
-            }, explode( ',', $tags ) );
+            }, explode( ',', $tags ) ); //Аналог split
 
-        $ip_provider = $this->get('provider:ip');
+        $ip_provider = $this->get('provider:ip'); //Вызов IPProvider.php для добавления адреса в пул
 
         try {
-            switch( $submit_type ) {
+            switch( $submit_type ) { //Определение количества запрошенных адресов
                 case 'single':
-                    $ip_provider->addIP( $ip, $tags );
+                    $ip_provider->addIP( $ip, $tags ); //Добавление адреса в пул
                     break;
                 case 'range':
-                    $ip_provider->addIPRange( $ip, intval( $amount ), $tags );
+                    $ip_provider->addIPRange( $ip, intval( $amount ), $tags ); //Добавление диапазона адресов в пул
                     break;
                 default:
-                    if( 'POST' === $_SERVER['REQUEST_METHOD'] ) {
+                    if( 'POST' === $_SERVER['REQUEST_METHOD'] ) { //Исключение на неправильное поле $submit_type
                         throw new \Exception('Invalid form data submitted!');
                     }
             }
-        } catch( \Exception $ex ) {
+        } catch( \Exception $ex ) { //Обработка исключения
             $data['error'] = $ex->getMessage();
         }
 
-        $ips = $ip_provider->getAllIPs();
+        $ips = $ip_provider->getAllIPs(); //Получение всего пула адресов
         foreach( $ips as &$ip_info ) {
-            sort( $ip_info['tags'] );
-            $ip_info['tags'] = implode( ',', $ip_info['tags'] );
+            sort( $ip_info['tags'] ); //Сортировка по полю 'tags'
+            $ip_info['tags'] = implode( ',', $ip_info['tags'] ); //Создание строки из массива
         }
-        unset( $ip_info );
+        unset( $ip_info ); //Удаление переменной $ip_info
 
-        usort( $ips, function ( array $ip_1, array $ip_2 ) {
-            return strcasecmp( $ip_1['tags'], $ip_2['tags'] );
+        usort( $ips, function ( array $ip_1, array $ip_2 ) { //Аналог qsort
+            return strcasecmp( $ip_1['tags'], $ip_2['tags'] ); //Сравнение строк без учета регистра
         } );
 
-        $data['IPs'] = [];
+        $data['IPs'] = []; //Объявление словаря $data с полем IPs
         foreach( $ips as $ip_info ) {
             $data['IPs'][] = [
-                'tags' => $ip_info['tags'],
+                'tags' => $ip_info['tags'], //Иморт полей tags IP и service_uris
                 'IP' => $ip_info['IP'],
                 'service_uris' => implode( ',<br>', array_map( function ( $service ) {
                     return '<a href="/admin/clientsservices.php?userid='.$service['userid'].'&id='.$service['id'].'" target="_blank">'.$service['id'].'</a>';
@@ -63,7 +63,7 @@ class AdminController extends AbstractController {
         return $data;
     }
 
-    public function defineTemplates() {
+    public function defineTemplates() { //Определение шаблона
         return [
             'output' => 'ip_table',
         ];
