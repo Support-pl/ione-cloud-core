273d632d1af136963ca303f15b76c95b2896605a
slnt_opp <nik.ivanovskij@gmail.com>
2017-05-22 00:21:43 +0300

    Изменения в тестах

--- Server/clienttest.rb
+++ Server/clienttest.rb
@@ -1,17 +1,9 @@
 require 'rubygems'
 require 'zmqjsonrpc'
 
-client = ZmqJsonRpc::Client.new("tcp://localhost:8080")
-ip, vmid = client.NewAccount("23411231", "nick.iwanowski@support.by", "Nhb500Gznmcjn", "ubuntu-15.04", 1, 1, 1024, 8192)
+client = ZmqJsonRpc::Client.new("tcp://185.66.70.215:8080")
+ip, vmid, userid = client.NewAccount("23411231", "kraken@support.by", "Nhb500Gznmcjn", "debian8", 1, 1, 1024, 8192)
 
 puts("IP: #{ip}")
 puts("VMID: #{vmid}")
-
-result = client.Terminate("nick.iwanowski@support.by", [126])
-puts(result)
-result = client.Terminate("nick.iwanowski@support.by", [123, 254])
-puts(result)
-
-ip, vmid = client.AddVM(6666666, "nick.iwanowski@support.by", 2, "debian8", 2, 2048, 16384)
-puts("IP: #{ip}")
-puts("VMID: #{vmid}")
+puts("UserID: #{userid}")
