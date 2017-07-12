d061203cbfc24d83fbb0cb12d61a8cbd8aa0827a
slnt_opp <nik.ivanovskij@gmail.com>
2017-07-12 18:34:57 +0300

    Kernel fixes and addons
    Fixed VM resize and Release
    Added:
     -Shutdown "Button"
     -Reboot functional - testing
     -Suspend fixed
     -IP-parser - testing

--- Server/service/ON_API/main.rb
+++ Server/service/ON_API/main.rb
@@ -10,18 +10,18 @@ def UserCreate(login, pass, quota, client)
     end
 
     if allocation_result != nil then errors.insert(0, allocation_result.message) end # В случае неудачного размещения будет ошибка, при удачном nil
-    chgrp_result = user.chgrp(157) # Смена группы пользователя ( 157 - группа PaaS )
+    chgrp_result = user.chgrp(1) # Смена группы пользователя ( 157 - группа PaaS )
     if chgrp_result != nil then errors.insert(0, chgrp_result.message) end # В случае ошибки при смене группы будет ошибка, порядок == nil 
     quota = user.set_quota(quota) # Настройка квоты пользователя
     if quota != nil then errors.insert(0, quota.message) end # В случае ошибки квотирования будет ошибка, успех == nil
     return user.id, errors
 end
 
-def VMCreate(login, userid, os, client, cpu = 2, memory = 1024)
+def VMCreate(login, billingid, userid, os, client, cpu = 2, memory = 1024)
     errors = Array.new(0, String)
     template = Template.new(Template.build_xml(os), client) # os - номер шаблона
     begin
-        vmid = template.instantiate(login + "_" + os, true) # деплой машины из шаблона №os, true означает, что машина не будет деплоится сразу, а создасться в состоянии HOLD
+        vmid = template.instantiate(login + "_" + billingid.to_s, true) # деплой машины из шаблона №os, true означает, что машина не будет деплоится сразу, а создасться в состоянии HOLD
     rescue => exception
         return exception.message
     end
@@ -29,15 +29,25 @@ def VMCreate(login, userid, os, client, cpu = 2, memory = 1024)
         puts vmid.message
     rescue => e
     end
-
     vm = VirtualMachine.new(VirtualMachine.build_xml(vmid), client)
-    chown_result = vm.chown(userid, 157) # Убедиться что срабатывает + настроить группы
-    if chown_result != nil then errors.insert(0, chown_result.message) end
+    chown_result = vm.chown(userid, 1) # Убедиться что срабатывает + настроить группы
+    if chown_result != nil
+        errors.insert(0, chown_result.message)
+    end
     vm.resize( # Надо тестить
         "CPU=\"#{cpu}\"
         MEMORY=\"#{memory}\"
-        VCPU=\"1\""
+        VCPU=\"1\"",
+        true
     )
-    vm.release # Непосредственно деплой, т.н. смена состояния с HOLD на ACTIVE
-    return vmid, errors
+    # vm.release # Непосредственно деплой, т.н. смена состояния с HOLD на ACTIVE
+    xml = Nokogiri::XML(vm.monitoring_xml)
+    sleep(1) until vm.lcm_state_str != "BOOT"
+    begin
+        ip = xml.xpath('//GUEST_IP').content #!!!!!
+    rescue => e
+        puts "[ #{time()} ] " + e.message
+        ip = "ERROR_OCCURRED"
+    end
+    return vmid, errors, ip
 end
