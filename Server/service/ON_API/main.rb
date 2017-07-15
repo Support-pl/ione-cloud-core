e0635a607d4ae80d6782edd570a8a80b7e375f15
slnt_opp <nik.ivanovskij@gmail.com>
2017-07-15 19:57:52 +0300

    Alpha v0.0.1
    * Фикс парсера IP-адреса. За парсинг отвечает отдельный метод ip(vmid).
    * Создан тест под парсер
    * Теперь все виды ошибок логгируются

--- Server/service/ON_API/main.rb
+++ Server/service/ON_API/main.rb
@@ -1,38 +1,37 @@
 def UserCreate(login, pass, quota, client)
-    errors = Array.new(0, String) # Массив message объектов OpenNebula::Error ошибок
     user = User.new(User.build_xml(100), client) # Генерирование объекта User на основе шаблонного пользователя группы PaaS
 
     begin
         allocation_result = user.allocate(login, pass) # Создание и размещение в пул нового пользователя login:pass
     rescue => e
-        errors.insert(0, e.message) # Обработка исключения "NAME is already taken by User ...."
-        return 0, errors
+        raise e.message
+        return 0
     end
 
-    if allocation_result != nil then errors.insert(0, allocation_result.message) end # В случае неудачного размещения будет ошибка, при удачном nil
+    if allocation_result != nil then puts allocation_result.message end # В случае неудачного размещения будет ошибка, при удачном nil
     chgrp_result = user.chgrp(1) # Смена группы пользователя ( 157 - группа PaaS )
-    if chgrp_result != nil then errors.insert(0, chgrp_result.message) end # В случае ошибки при смене группы будет ошибка, порядок == nil 
+    if chgrp_result != nil then raise chgrp_result.messages end # В случае ошибки при смене группы будет ошибка, порядок == nil 
     quota = user.set_quota(quota) # Настройка квоты пользователя
-    if quota != nil then errors.insert(0, quota.message) end # В случае ошибки квотирования будет ошибка, успех == nil
-    return user.id, errors
+    if quota != nil then puts quota.message end # В случае ошибки квотирования будет ошибка, успех == nil
+    return user.id
 end
 
 def VMCreate(login, billingid, userid, os, client, cpu = 2, memory = 1024)
-    errors = Array.new(0, String)
     template = Template.new(Template.build_xml(os), client) # os - номер шаблона
     begin
         vmid = template.instantiate(login + "_" + billingid.to_s, true) # деплой машины из шаблона №os, true означает, что машина не будет деплоится сразу, а создасться в состоянии HOLD
     rescue => exception
-        return exception.message
+        raise exception.message
+        return 0
     end
     begin
-        puts vmid.message
+        raise vmid.message
     rescue => e
     end
     vm = VirtualMachine.new(VirtualMachine.build_xml(vmid), client)
     chown_result = vm.chown(userid, 1) # Убедиться что срабатывает + настроить группы
     if chown_result != nil
-        errors.insert(0, chown_result.message)
+        raise chown_result.message
     end
     vm.resize( # Надо тестить
         "CPU=\"#{cpu}\"
@@ -40,14 +39,6 @@ def VMCreate(login, billingid, userid, os, client, cpu = 2, memory = 1024)
         VCPU=\"1\"",
         true
     )
-    # vm.release # Непосредственно деплой, т.н. смена состояния с HOLD на ACTIVE
-    xml = Nokogiri::XML(vm.monitoring_xml)
-    sleep(1) until vm.lcm_state_str != "BOOT"
-    begin
-        ip = xml.xpath('//GUEST_IP').content #!!!!!
-    rescue => e
-        puts "[ #{time()} ] " + e.message
-        ip = "ERROR_OCCURRED"
-    end
-    return vmid, errors, ip
+    vm.release # Непосредственно деплой, т.н. смена состояния с HOLD на ACTIVE
+    return vmid
 end
--- Server/service/handlers/WHMCS.rb
+++ Server/service/handlers/WHMCS.rb
@@ -8,21 +8,14 @@ class WHMHandler
 
     def NewAccount(billingid, login, pass, vmquota, os, cpu, memory, disk) # Хэндлер создания нового аккаунта PaaS и деплой машины в него
         puts "[ #{time()} ] New Account for #{billingid} Order Accepted!"
-        # login = String.new()
-        #pass = Passgen::generate( :length => 12, :symbols => true)
-        # for i in 0..logb.size do # Создание логина на основе почты
-        #     if logb[i] == "@" then break end
-        #     login += logb[i]
-        # end
-
         puts "[ #{time()} ] Generating Quota for #{billingid}"
         quota = NewQuota(login, vmquota, cpu, memory, disk) # Генерирование квоты для нового пользователя
         puts "[ #{time()} ] Creating new user for #{billingid} - #{login}"
-        userid, errors = UserCreate(login, pass, quota, @client)
+        userid = UserCreate(login, pass, quota, @client)
         puts "[ #{time()} ] Creating VM for #{billingid}"
-        vmid, buff, ip = VMCreate(login, billingid, userid, os, @client, cpu, memory) # Получение vmid только что созданной машины
-        if buff != nil then errors += buff end
-        return ip, vmid, userid, errors # Возврат в WHMCS IP-адреса и VMID машины, ID пользователя ON и массив ошибок
+        vmid = VMCreate(login, billingid, userid, os, @client, cpu, memory) # Получение vmid только что созданной машины
+        ip = ip(vmid)
+        return ip, vmid, userid # Возврат в WHMCS IP-адреса и VMID машины, ID пользователя ON
     end
     def Suspend(userid, vmid)
         puts "[ #{time()} ] Suspending VM#{vmid}"
@@ -63,13 +56,6 @@ class WHMHandler
             Kernel.abort("[ #{time()} ] Server Stopped Remotely")
         end
     end
-    def Resume(vmid)
-        vm = VirtualMachine.new(VirtualMachine.build_xml(vmid), @client)
-        if vm.lcm_state_str = "shut" then
-            vm.resume
-            return
-        end
-    end
     def VM_XML(vmid)
         vm = VirtualMachine.new(VirtualMachine.build_xml(vmid), @client)
         return vm.monitoring_xml
@@ -82,5 +68,11 @@ class WHMHandler
     def Resume(vmid)
         vm = VirtualMachine.new(VirtualMachine.build_xml(vmid), @client)
         vm.resume
+    end
+    def ip(vmid)
+        doc = Nokogiri::XML(VM_XML(vmid))
+        address = ""
+        doc.xpath('//GUEST_IP').each do |content| address = content.text.to_s end
+        return address
     end
 end
