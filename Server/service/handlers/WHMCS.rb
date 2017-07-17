a61024392b2fe118e9894a0a67df04b72bdb4bec
slnt_opp <nik.ivanovskij@gmail.com>
2017-07-17 16:41:40 +0300

    Update v0.1.3
    * Фикс метода Suspend(требует тестирования логика работы)
    * Создан TODO.md список дел в корне проекта сервера

--- Server/service/handlers/WHMCS.rb
+++ Server/service/handlers/WHMCS.rb
@@ -15,24 +15,17 @@ class WHMHandler
         ip = GetIP(vmid)
         return ip, vmid, userid
     end
-    # def NewAccount(billingid, login, pass, vmquota, os, cpu, memory, disk) # Хэндлер создания нового аккаунта PaaS и деплой машины в него
-    #     puts "[ #{time()} ] New Account for #{billingid} Order Accepted!"
-    #     puts "[ #{time()} ] Generating Quota for #{billingid}"
-    #     quota = NewQuota(login, vmquota, cpu, memory, disk) # Генерирование квоты для нового пользователя
-    #     puts "[ #{time()} ] Creating new user for #{billingid} - #{login}"
-    #     userid = UserCreate(login, pass, quota, @client)
-    #     puts "[ #{time()} ] Creating VM for #{billingid}"
-    #     vmid = VMCreate(login, billingid, userid, os, @client, cpu, memory) # Получение vmid только что созданной машины
-    #     ip = GetIP(vmid)
-    #     return ip, vmid, userid # Возврат в WHMCS IP-адреса и VMID машины, ID пользователя ON
-    # end
-    def Suspend(userid, vmid)
+    def Suspend(userid, vmid = nil)
+        puts "Suspend query for User##{userid} Accepted!"
+        puts "[ #{time()} ] Changing group of user #{userid} to SuspendedUsers"
+        user = User.new(User.build_xml(userid), @client)
+        user.chgrp(103) # Поправить число на номер группы SuspendedUsers
+        if vmid == nil then
+            return nil
+        end
         puts "[ #{time()} ] Suspending VM#{vmid}"
         vm = VirtualMachine.new(VirtualMachine.build_xml(vmid), @client)
         vm.suspend
-        # puts "[ #{time()} ] Changing group of user #{userid} to SuspendedUsers"
-        # user = User.new(User.build_xml(userid), @client)
-        # user.chgrp(3) # Поправить число на номер группы SuspendedUsers
     end
     def Reboot(vmid)
         puts "[ #{time()} ] Rebooting VM#{vmid}"
@@ -60,12 +53,6 @@ class WHMHandler
         user = User.new(User.build_xml(userid), @client)
         user.delete
     end
-    # def StopServer(passwd)
-    #     if(passwd == "Jago322==") then
-    #         puts "[ #{time()} ] Server Stopped Manualy"
-    #         Kernel.abort("[ #{time()} ] Server Stopped Remotely")
-    #     end
-    # end
     def VM_XML(vmid)
         vm = VirtualMachine.new(VirtualMachine.build_xml(vmid), @client)
         return vm.monitoring_xml
@@ -85,4 +72,22 @@ class WHMHandler
         doc.xpath('//GUEST_IP').each do |content| address = content.text.to_s end
         return address
     end
+    # def NewAccount(billingid, login, pass, vmquota, os, cpu, memory, disk) # Хэндлер создания нового аккаунта PaaS и деплой машины в него
+    #     puts "[ #{time()} ] New Account for #{billingid} Order Accepted!"
+    #     puts "[ #{time()} ] Generating Quota for #{billingid}"
+    #     quota = NewQuota(login, vmquota, cpu, memory, disk) # Генерирование квоты для нового пользователя
+    #     puts "[ #{time()} ] Creating new user for #{billingid} - #{login}"
+    #     userid = UserCreate(login, pass, quota, @client)
+    #     puts "[ #{time()} ] Creating VM for #{billingid}"
+    #     vmid = VMCreate(login, billingid, userid, os, @client, cpu, memory) # Получение vmid только что созданной машины
+    #     ip = GetIP(vmid)
+    #     return ip, vmid, userid # Возврат в WHMCS IP-адреса и VMID машины, ID пользователя ON
+    # end
+
+    # def StopServer(passwd)
+    #     if(passwd == "Jago322==") then
+    #         puts "[ #{time()} ] Server Stopped Manualy"
+    #         Kernel.abort("[ #{time()} ] Server Stopped Remotely")
+    #     end
+    # end
 end
