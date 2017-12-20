ef8407f2b30d279dcf386f010d85337d7ccd607f
slnt_opp <nik.ivanovskij@gmail.com>
2017-12-20 19:01:51 +0300

    Auto stash before merge of "stable" and "origin/stable"

--- Server/lib/data_import_handler/main.rb
+++ Server/lib/data_import_handler/main.rb
@@ -1,5 +1,10 @@
 class WHMHandler
     def IMPORT(username, credentials, vmid, group)
+        LOG username, 'DEBUG'
+        LOG credentials, 'DEBUG'
+        LOG vmid, 'DEBUG'
+        LOG group, 'DEBUG'
+        return nil
         userid = UserCreate(username, credentials, group, @client)
         vm = get_pool_element(VirtualMachine, vmid, @client)
         vm.chown(userid, USERS_GROUP)
